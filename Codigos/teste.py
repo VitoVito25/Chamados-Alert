f39fd85406ce76327dde25e4c60a51683f85c426
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-08-20 08:47:27 -0300

    testando localizar chamados

--- Codigos/teste.py
+++ Codigos/teste.py
@@ -21,11 +21,30 @@ navegador.find_element('xpath','/html/body/div/div/div/form/div/div[2]/div[3]/bu
 Select(navegador.find_element('xpath','//*[@id="page-wrapper"]/div[2]/div[1]/form/div/div[2]/div[2]/select')).select_by_value('3053')
 navegador.find_element('xpath', '//*[@id="page-wrapper"]/div[2]/div[1]/form/div/div[1]/button').click()
 
+#####
+
 # Lista de termos para busca
 termos_para_busca = ["TRAMITE 5.0", "ALMOX 5.0", "SCF 5.0", "SPT 5.0", "SRH 5.0"]
 
 # Lista para armazenar os conteúdos de td[2]
 conteudos_encontrados = []
+
+# Iterar por todas as linhas da tabela
+linhas = navegador.find_elements('xpath', '//*[@id="tableMeusChamados"]/tbody/tr')
+totalLinhas = enumerate(linhas, start=1)
+
+i = 1
+for i, linha in totalLinhas:
+    # Obter o conteúdo da terceira célula (td[3])
+    td3_text = linha.find_element('xpath', f'//*[@id="tableMeusChamados"]/tbody/tr[{i}]/td[3]').text
+    
+    # Verificar se o texto de td[3] corresponde a algum dos termos buscados
+    if td3_text in termos_para_busca:
+        linha.find_element('xpath', f'//*[@id="tableMeusChamados"]/tbody/tr[{i}]/td[3]').click
+        input("Pause...")
+
+
+#####
 
 #Deixando o navegador aberto até "Enter"
 input("Pressione Enter para fechar o navegador...")
