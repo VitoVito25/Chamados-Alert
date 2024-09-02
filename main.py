429fb52acd9124f421dfc29b3ab9d72d69ced65d
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-09-02 16:58:37 -0300

    informando hora de atualização

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from functions import get_credentials, start_browser, access_colaborador, search_tickets, display_results, get_search_interval, check_and_install_packages, print_art
 from utils import clear_console
+from datetime import datetime
 import time
 
 def main():
@@ -18,6 +19,7 @@ def main():
     print_art()
     systems_to_search = ["TRAMITE 5.00", "ALMOX 5.00", "SCF 5.00", "STP 5.00", "BI 5.00"]
     print("Buscando pelos sistemas: ", ' - '.join(systems_to_search))
+    print(f"Pressione 'Ctrl + C' para finalizar o programa a qualquer momento")
 
     try:
         while True:
@@ -25,9 +27,11 @@ def main():
             access_colaborador(browser, username, password)
             found_contents = search_tickets(browser, systems_to_search)
             display_results(found_contents)
-            print(f"Aguardando {search_interval_min} minutos para a próxima busca...")
-            print(f"Pressione 'Ctrl + C' para finalizar o programa ")
-            time.sleep(search_interval_sec)
+
+            # Obter a hora atual
+            hora_atual = datetime.now().strftime("%H:%M:%S")
+            print(f"[{hora_atual}] Aguardando {search_interval_min} minutos para a próxima busca...")
+            time.sleep(10)
 
             # Limpa o vetor para sempre aparecer os mesmos dados
             found_contents.clear()
