698972bef5318274bfe2bb482481adf1e583eb14
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-09-11 14:37:04 -0300

    Import options

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import WebDriverException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from plyer import notification
@@ -12,7 +13,7 @@ from datetime import datetime
 import getpass
 
 # Variável global para armazenar a instância do navegador
-browser = None
+browser = None    
 
 def start_browser():
     global browser  # Utiliza a variável global 'browser'
